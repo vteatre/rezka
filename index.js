4b008dec8ffc7fe3166cee94f2bb2b5635adc2a1
vteatre <72284059+vteatre@users.noreply.github.com>
2020-10-04 01:56:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,11 +40,11 @@ console.time('DONE');
 
 intervalId = setInterval(save, 10000);
 
-const loop1 = JSON.parse(JSON.stringify(Array.from(Array(1300).keys())));
+const loop1 = JSON.parse(JSON.stringify(Array.from(Array(1200).keys())));
 const loop2 = JSON.parse(JSON.stringify(Array.from(Array(1000).keys())));
 
 async.eachOfLimit(loop1, 15, function (key, index, callback) {
-    if (key < 1200) return callback();
+    if (key < 1100) return callback();
     async.eachOfLimit(loop2, 1000, function (key2, index, callback) {
         const id = key * 1000 + key2;
         const request = https
