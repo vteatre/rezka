fb99747327d75314a0393fb70921497963be4bb0
vteatre <72284059+vteatre@users.noreply.github.com>
2020-10-03 04:23:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const loop1 = JSON.parse(JSON.stringify(Array.from(Array(1300).keys())));
 const loop2 = JSON.parse(JSON.stringify(Array.from(Array(1000).keys())));
 
 async.eachOfLimit(loop1, 15, function (key, index, callback) {
-    if (key < 1270) return callback();
+    if (key < 1260) return callback();
     console.log(key);
     async.eachOfLimit(loop2, 1000, function (key2, index, callback) {
         const id = key * 1000 + key2;
